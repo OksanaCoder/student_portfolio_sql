e0d596ac6cf48761b0d30c67fbcd4aa49d01efd0
Oksana <myzove@mail.ru>
2020-07-16 14:34:31 +0300

    adding front

--- routes/users/index.js
+++ routes/users/index.js
@@ -6,6 +6,7 @@ const studentRoute = express.Router()
 studentRoute.get("/", async(req, res)=>{
     const response = await db.query('SELECT _id, name, surname, email, dateofbirth FROM students')
     res.send(response.rows)
+
 })
 studentRoute.get("/:id", async(req, res)=>{
     const response = await db.query('SELECT _id, name, surname, email, dateofbirth FROM students WHERE _id = $1', [req.params.id])
@@ -23,22 +24,25 @@ studentRoute.post("/", async(req, res)=>{
 })
 studentRoute.put("/:id", async(req, res)=>{
     try {
-        const result = await db.query(`UPDATE students
-                                        SET name = $1,
-                                            surname = $2,
-                                            email = $3,
-                                            dateofbirth = $4
-                                            WHERE _id = $5
-                                            RETURNING * `,
-                                            [req.body.name, req.body.surname, req.body.email, req.body.dateOfBirth, req.params.id ])
-                                           
-            if(result.rowCount === 0)
-            return res.status(400).send('Not found')
-
-            res.send(result[0])
-    } catch(ex) {
-            console.log(ex)
-            res.status(500).send(ex)
+        let params = []
+        let query = 'UPDATE "students" SET '
+        for (bodyParamName in req.body) {
+            query += // for each element in the body I'll add something like parameterName = $Position
+                (params.length > 0 ? ", " : '') + //I'll add a coma before the parameterName for every parameter but the first
+                bodyParamName + " = $" + (params.length + 1) // += Category = $1 
+            params.push(req.body[bodyParamName]) //save the current body parameter into the params array
+        }
+        params.push(req.params.id) //push the asin into the array
+        query += " WHERE _id = $" + (params.length) + " RETURNING *" //adding filtering for ASIN + returning
+        console.log(query)
+        const result = await db.query(query, params) //querying the DB for updating the row
+        if (result.rowCount === 0) //if no element match the specified ASIN => 404
+            return res.status(404).send("Not Found")
+        res.send(result.rows[0]) //else, return the updated version
+    }
+    catch(ex) {
+        console.log(ex)
+        res.status(500).send(ex)
     }
 
 })
